0ab31780b126db1f2ea8db917371c072deff6980
Kudzeri <kudzeri@gmail.com>
2024-07-17 15:16:56 +0500

    init project

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="Almaty"
+
+ENTRYPOINT ["top", "-b"]
---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+}
