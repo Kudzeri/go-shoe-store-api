726e9ae6699383d1b45eb3f9791d4dffe6e4bb5b
Kudzeri <kudzeri@gmail.com>
2024-07-18 10:46:11 +0500

    add functionality to User repository

--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -1,1 +1,28 @@
 package repositories
+
+import (
+	"github.com/Kudzeri/go-shoe-store-api/config"
+	"github.com/Kudzeri/go-shoe-store-api/models"
+)
+
+func CreateUser(user *models.User) error {
+	result := config.DB.Create(&user)
+
+	return result.Error
+}
+
+func FindUserByID(id string) (models.User, error) {
+	var user models.User
+	result := config.DB.First(&user, "id = ?", id)
+	return user, result.Error
+}
+
+func UpdateUser(id string, user *models.User) error {
+	result := config.DB.Model(&user).Where("ud = ?", id).Updates(user)
+	return result.Error
+}
+
+func DeleteUser(id string) error {
+	result := config.DB.Delete(&models.User{}, id)
+	return result.Error
+}
