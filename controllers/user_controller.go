b4f0996530c0a1f81ecca6566d3972d1ac26fc75
Kudzeri <kudzeri@gmail.com>
2024-07-18 10:59:10 +0500

    add functionality to User controller

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,1 +1,54 @@
 package controllers
+
+import (
+	"github.com/Kudzeri/go-shoe-store-api/models"
+	"github.com/Kudzeri/go-shoe-store-api/services"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func RegisterUser(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := services.RegisterUser(&user); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, user)
+}
+
+func GetUser(c *gin.Context) {
+	id := c.Param("id")
+	user, err := services.GetUserByID(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func UpdateUser(c *gin.Context) {
+	id := c.Param("id")
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := services.UpdateUser(id, &user); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func DeleteUser(c *gin.Context) {
+	id := c.Param("id")
+	if err := services.DeleteOrder(id); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "User deleted"})
+}
