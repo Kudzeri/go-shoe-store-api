645d909c47c980a503e79be8c92d07ecdcd4477c
Kudzeri <kudzeri@gmail.com>
2024-07-18 10:21:49 +0500

    ad functionality to Order service

--- services/order_service.go
+++ services/order_service.go
@@ -1,1 +1,26 @@
 package services
+
+import (
+	"github.com/Kudzeri/go-shoe-store-api/models"
+	"github.com/Kudzeri/go-shoe-store-api/repositories"
+)
+
+func GetAllOrders() ([]models.Order, error) {
+	return repositories.FindAllOrders()
+}
+
+func GetOrderById(id string) (models.Order, error) {
+	return repositories.FindOrderByID(id)
+}
+
+func CreateOrder(order *models.Order) error {
+	return repositories.CreateOrder(order)
+}
+
+func UpdateOrder(id string, product *models.Order) error {
+	return repositories.UpdateOrder(id, product)
+}
+
+func DeleteOrder(id string) error {
+	return repositories.DeleteOrder(id)
+}
