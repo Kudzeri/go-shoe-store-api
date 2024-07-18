4439b14503ff18f9ef1982010ffacc6a70b4ab2f
Kudzeri <kudzeri@gmail.com>
2024-07-18 10:49:53 +0500

    add functionality to User service

--- services/user_service.go
+++ services/user_service.go
@@ -1,1 +1,22 @@
 package services
+
+import (
+	"github.com/Kudzeri/go-shoe-store-api/models"
+	"github.com/Kudzeri/go-shoe-store-api/repositories"
+)
+
+func RegisterUser(user *models.User) error {
+	return repositories.CreateUser(user)
+}
+
+func GetUserByID(id string) (models.User, error) {
+	return repositories.FindUserByID(id)
+}
+
+func UpdateUser(id string, user *models.User) error {
+	return repositories.UpdateUser(id, user)
+}
+
+func DeleteUser(id string) error {
+	return repositories.DeleteUser(id)
+}
