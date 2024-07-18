d42b0cf7e002a5e94d2933edf78ef79810a8ebd6
Kudzeri <kudzeri@gmail.com>
2024-07-18 11:54:07 +0500

    add GenerateJWT && ValidateJWT

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,1 +1,42 @@
 package utils
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+var jwtKey = []byte("secret2333@xxcsa@aaAs")
+
+func GenerateJWT(username string) (string, error) {
+	expirationTime := time.Now().Add(1 * time.Hour)
+
+	claims := &jwt.StandardClaims{
+		ExpiresAt: expirationTime.Unix(),
+		IssuedAt:  time.Now().Unix(),
+		Subject:   username,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func ValidateJWT(tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return jwtKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
