82c2025331c97a51846a28b81d7d9918465776ab
Kudzeri <kudzeri@gmail.com>
2024-07-18 18:57:16 +0500

    add product table migration

--- db/migrations/000001_create_products_table.up.sql
+++ db/migrations/000001_create_products_table.up.sql
@@ -1,8 +1,12 @@
 CREATE TABLE products (
                           id SERIAL PRIMARY KEY,
-                          name VARCHAR(255) NOT NULL,
+                          name VARCHAR(100) NOT NULL,
                           description TEXT,
-                          price DECIMAL(10, 2) NOT NULL,
+                          price DECIMAL(10,2) NOT NULL,
+                          stock INT NOT NULL,
+                          category VARCHAR(50) NOT NULL,
                           created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-                          updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+                          updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+                          deleted_at TIMESTAMP
 );
+
