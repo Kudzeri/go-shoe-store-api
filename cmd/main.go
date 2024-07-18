c64b4d786d97bce17906f6577017676e965cee65
Kudzeri <kudzeri@gmail.com>
2024-07-18 09:30:08 +0500

    init project

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/Kudzeri/go-shoe-store-api/config"
+	"github.com/gin-gonic/gin"
 	"log"
 	"os"
 )
@@ -12,4 +14,10 @@ func main() {
 		port = "8080"
 		log.Printf("Defaulting to port %s", port)
 	}
+
+	router := gin.Default()
+	config.ConnectDB()
+	routes.SetupRoutes(router)
+
+	router.Run(":" + port)
 }
