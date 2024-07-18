434d3ff07aadbf9680dccd2fbd96b92448403c09
Kudzeri <kudzeri@gmail.com>
2024-07-18 09:53:02 +0500

    add product service

--- services/product_service.go
+++ services/product_service.go
@@ -1,1 +1,26 @@
 package services
+
+import (
+	"github.com/Kudzeri/go-shoe-store-api/controllers"
+	"github.com/Kudzeri/go-shoe-store-api/models"
+)
+
+func GetAllProducts() ([]models.Product, error) {
+	return controllers.FindAllProducts()
+}
+
+func GetProductByID(id string) (models.Product, error) {
+	return controllers.FindProductByID(id)
+}
+
+func CreateProduct(product *models.Product) error {
+	return controllers.CreateProduct(product)
+}
+
+func UpdateProduct(id string, product *models.Product) error {
+	return controllers.UpdateProduct(id, product)
+}
+
+func DeleteProduct(id string) error {
+	return controllers.DeleteProduct(id)
+}
