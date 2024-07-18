203f5769ff7e19a7b999b0706023c9bd719a6fb6
Kudzeri <kudzeri@gmail.com>
2024-07-18 09:30:36 +0500

    init database connection

--- config/config.go
+++ config/config.go
@@ -1,1 +1,19 @@
 package config
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() *gorm.DB {
+	dsn := "host=localhost user=postgres password=postgres dbname=mydb port=5432 sslmode=disable"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	DB = db
+}
