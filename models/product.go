533342fd3b122545f5dfeabe243ca8dd6a617989
Kudzeri <kudzeri@gmail.com>
2024-07-18 11:06:17 +0500

    add Product model

--- models/product.go
+++ models/product.go
@@ -6,4 +6,9 @@ import (
 
 type Product struct {
 	gorm.Model
+	Name        string  `json:"name" gorm:"type:varchar(100);not null"`
+	Description string  `json:"description" gorm:"type:text"`
+	Price       float64 `json:"price" gorm:"type:decimal(10,2);not null"`
+	Stock       int     `json:"stock" gorm:"type:int(11);not null"`
+	Category    string  `json:"category" gorm:"type:varchar(50);not null"`
 }
