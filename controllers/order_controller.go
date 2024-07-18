1eecda783a228c1edfc354ce6e3775719ab1096d
Kudzeri <kudzeri@gmail.com>
2024-07-18 10:34:09 +0500

    ad functionality to Order controller

--- controllers/order_controller.go
+++ controllers/order_controller.go
@@ -1,1 +1,60 @@
 package controllers
+
+import (
+	"github.com/Kudzeri/go-shoe-store-api/models"
+	"github.com/Kudzeri/go-shoe-store-api/services"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func GetOrders(c *gin.Context) {
+	orders, err := services.GetAllOrders()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, orders)
+}
+
+func GetOrder(c *gin.Context) {
+	id := c.Param("id")
+	order, err := services.GetOrderById(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, order)
+}
+
+func CreateOrder(c *gin.Context) {
+	var order models.Order
+	if err := c.ShouldBindJSON(&order); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := services.CreateOrder(&order); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, order)
+}
+
+func UpdateOrder(c *gin.Context) {
+	id := c.Param("id")
+	var order models.Order
+	if err := c.ShouldBindJSON(&order); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+	if err := services.UpdateOrder(id, &order); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, order)
+}
+
+func DeleteOrder(c *gin.Context) {
+	id := c.Param("id")
+	if err := services.DeleteOrder(id); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Order deleted successfully"})
+}
